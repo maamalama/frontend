03f563327dba330682079766fc5081fb23e17035
v1rtl <pilll.pl22@gmail.com>
2022-02-07 00:11:18 +0200

    push favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../global.css'
 
 const App = ({ pageProps, Component }: AppProps) => (
   <>
+    <Head>
+      <link rel="icon" href="/logo_favicon.png" type="image/png" />
+      <title>Hashscan - web3 user analytics</title>
+    </Head>
     <Component {...pageProps} />
   </>
 )
