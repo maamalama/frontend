ceb05919bbe3edc627ea97a0683367d83bf09df4
v1rtl <pilll.pl22@gmail.com>
2022-02-06 19:33:09 +0200

    add mock options

--- components/AddFilter.tsx
+++ components/AddFilter.tsx
@@ -9,11 +9,18 @@ import { useOnClickOutside } from '../hooks/useOnClickOutside'
 
 const styles = { ...styles1, ...styles2, ...sharedStyles }
 
-type Options = { label: string; value: FilterType; caption: string }
+type Options = { label: string; value: FilterType | false; caption: string }
 
 const options: Options[] = [
   { label: 'Hold NFTs', value: 'nft', caption: 'Users who have NFT(s) in their wallet' },
-  { label: 'Own ERC-20 tokens', value: 'erc20', caption: 'Users who own ERC-20 tokens' }
+  { label: 'Own ERC-20 tokens', value: 'erc20', caption: 'Users who own ERC-20 tokens' },
+  { label: 'Owns ENS', value: false, caption: 'Owns ENS domain name' },
+  { label: 'OpenSea trader', value: false, caption: 'Traded on OpenSea' },
+  {
+    label: 'Import users from CSV',
+    caption: 'import users from CSV',
+    value: false
+  }
 ]
 
 export const AddFilter = () => {
@@ -38,20 +45,25 @@ export const AddFilter = () => {
           <button
             role="listitem"
             className={styles.filterButton}
+            style={{ cursor: option.value === false ? 'not-allowed' : 'pointer' }}
+            disabled={option.value === false}
             onClick={() => {
-              if (option.value === 'nft') {
-                addFilter({
-                  ...collections[0],
-                  amount: '0',
-                  type: 'nft'
-                })
-              } else if (option.value === 'erc20') {
-                addFilter({
-                  ...tokens[0],
-                  amount: '0',
-                  type: 'erc20'
-                })
+              if (option.value) {
+                if (option.value === 'nft') {
+                  addFilter({
+                    ...collections[0],
+                    amount: '0',
+                    type: 'nft'
+                  })
+                } else if (option.value === 'erc20') {
+                  addFilter({
+                    ...tokens[0],
+                    amount: '0',
+                    type: 'erc20'
+                  })
+                }
               }
+
               setExpanded(false)
             }}
           >
