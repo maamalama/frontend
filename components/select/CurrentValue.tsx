d3688f0cfbcfe841b4ea3a49dbc42fa8ed973ce3
v1rtl <pilll.pl22@gmail.com>
2022-02-06 14:38:40 +0200

    restructure project and add events

---
+++ components/select/CurrentValue.tsx
@@ -0,0 +1,41 @@
+import type { SingleValueProps } from 'react-select'
+import { components } from 'react-select'
+import { networks } from '../../data/networks'
+import styles from './Option.module.css'
+import sharedStyles from '../../shared.module.css'
+import type { Option } from '../../lib/types'
+
+const { SingleValue } = components
+
+export const CurrentValue = (props: SingleValueProps<Option>) => {
+  return (
+    <SingleValue {...props}>
+      <span className={`${sharedStyles.row}`}>
+        <span className={`${styles.collectionContainer}`}>
+          {props.data.logo && (
+            <img
+              className={styles.collectionIcon}
+              src={props.data.logo}
+              height={24}
+              width={24}
+              alt={props.data.symbol}
+            />
+          )}
+          {props.data.chainId && (
+            <img
+              height={12}
+              width={12}
+              style={{
+                bottom: '0',
+                right: '7.5px'
+              }}
+              className={styles.chainIdIcon}
+              src={networks.find((chainId) => chainId.value === props.data.chainId)?.logo}
+            />
+          )}
+        </span>
+        {props.data.label}
+      </span>
+    </SingleValue>
+  )
+}
