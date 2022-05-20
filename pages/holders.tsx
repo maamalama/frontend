45036086c44ce5e8f23dd495fb5ff6140bd8315e
xamgore <xamgore@ya.ru>
2022-05-20 23:53:18 +0400

    Show

--- pages/holders.tsx
+++ pages/holders.tsx
@@ -1,19 +1,31 @@
-import styles from '../shared.module.css'
 import indexStyles from './index.module.css'
 import shared from '../shared.module.css'
+import css from './holders.module.css'
+import search from '../public/search.svg'
 
 const Holders = () => {
+  let nft = {
+    icon: 'https://etherscan.io/token/images/lobsterdao_32.png',
+    name: 'lobsterdao',
+  }
+
   return (
-    <main className={`${styles.column} ${indexStyles.main}`}>
+    <div className={`${shared.column} ${indexStyles.main}`}>
       <header className={shared.content_header}>
         <h1 className={shared.content_header__title}>Holders</h1>
         <div className={shared.content_header__chain}>
-          <span className={shared.content_header__chain_icon} style={{backgroundImage: 'url(https://etherscan.io/token/images/lobsterdao_32.png)'}}/>
-          lobsterdao
+          <span className={shared.content_header__chain_icon} style={{ backgroundImage: `url(${nft.icon})` }}/>
+          {nft.name}
         </div>
       </header>
 
-    </main>
+      <main className={css.content}>
+        <div className={css.search_bar}>
+          <img src={'/search.svg'} width={32} height={32} className={css.search_icon} alt=""/>
+          <input type="text" placeholder="Search by address or ENS" className={css.search_input}/>
+        </div>
+      </main>
+    </div>
   )
 }
 
