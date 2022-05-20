d1c3d86de1ef8aab89e65f1c6f0b8330cc94cf28
xamgore <xamgore@ya.ru>
2022-05-21 02:09:28 +0400

    Replace button with a link

--- pages/analytics.tsx
+++ pages/analytics.tsx
@@ -111,7 +111,7 @@ const NftAnalyticsPage = () => {
             <div className={`${sharedStyles.container} ${css.container} ${css.social_media_stats}`}>
               {/*<div><img src={'/discord.ico'} width={18} height={18}/>&nbsp;&nbsp;2,408 members</div>*/}
               <div>
-                <button style={{cursor: 'pointer', padding: '4px'}}><img src={'/discord.ico'} width={15} height={15}/> Connect Discord to view analytics</button>
+                <img src={'/discord.ico'} width={15} height={15}/> <a href={'/connect'} style={{color: '#2684FF'}}>Connect Discord to view analytics</a>
               </div>
               <div><img src={'/twitter.png'} width={18} height={18}/>&nbsp;&nbsp;14,213 followers</div>
             </div>
