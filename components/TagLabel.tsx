3b28d2e927f68577032f07b4cf4cbfc3e7cd5bc4
xamgore <xamgore@ya.ru>
2022-05-24 20:24:13 +0400

    Make filters to affect holders table

--- components/TagLabel.tsx
+++ components/TagLabel.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
 import css from './TagLabel.module.css'
 
-export const TagLabel = ({ children, initialIsActive = false }) => {
-  let [isActive, setIsActive] = useState(initialIsActive)
+export const TagLabel = ({ children, isActive, onClick }) => {
   return (
-    <span className={`${css.tag_label} ${isActive ? css.tag_label__active : ''}`} onClick={() => setIsActive(!isActive)}>{children}</span>
+    <span
+      className={`${css.tag_label} ${isActive ? css.tag_label__active : ''}`}
+      onClick={() => onClick(!isActive)}
+    >
+      {children}
+    </span>
   )
 }
