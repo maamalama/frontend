381ffe8c25da9708abbd8a2dffda5e2cb0174dd0
xamgore <xamgore@ya.ru>
2022-05-26 09:14:32 +0400

    Omit numeric buttons in the pagination

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -4,12 +4,12 @@ import css from './Pagination.module.css'
 type ButtonProps = {
   page: number
   children: ReactNode,
-  onClick: (page: number) => any,
+  onClick?: (page: number) => any,
   className?: string,
 }
 
 const Button = ({ page, children, onClick, className }: ButtonProps) => {
-  return <span className={`${css.pagination__button} ${className}`} onClick={() => onClick(page)}>{children}</span>
+  return <span className={`${css.pagination__button} ${className}`} onClick={() => onClick?.(page)}>{children}</span>
 }
 
 export const Pagination = ({ page, total, onSelect }) => {
@@ -17,35 +17,21 @@ export const Pagination = ({ page, total, onSelect }) => {
 
   let hasLeft = page !== 0
   let hasRight = page !== total - 1
-  // (<< < 5 слева)?, текущую, (5 справа > >>)?
 
-  if (page === 0) {
-    '1 2 3 4 5 6 > >>'
-  }
+  let first = 0, last = total - 1
+  let prev = Math.max(first, page - 1)
+  let next = Math.min(last, page + 1)
 
+  // [First] [<] [Page 1 of 12] [>] [Last]
   return (
     <div className={css.pagination}>
-      {hasLeft && <>
-        <Button page={0} onClick={onSelect}>{'≪'}</Button>
-        <Button page={page - 1} onClick={onSelect}>{'<'}</Button>
-        {range(Math.max(0, page - 5), Math.max(0, page - 1))
-          .map(i => <Button key={i} page={i} onClick={onSelect}>{i + 1}</Button>)}
-      </>}
+      <Button page={first} className={hasLeft && css.pagination__active} onClick={onSelect}>First</Button>
+      <Button page={prev} className={hasLeft && css.pagination__active} onClick={onSelect}>{'‹'}</Button>
 
-      <Button page={page} className={css.pagination__active} onClick={onSelect}>{page + 1}</Button>
+      <Button page={page} className={css.pagination__button__wide}>{`Page ${page + 1} of ${total}`}</Button>
 
-      {hasRight && <>
-        {range(Math.min(total - 1, page + 1), Math.min(total - 1, page + 5))
-          .map(i => <Button key={i} page={i} onClick={onSelect}>{i + 1}</Button>)}
-        <Button page={page + 1} onClick={onSelect}>{'>'}</Button>
-        <Button page={total - 1} onClick={onSelect}>{'≫'}</Button>
-      </>}
+      <Button page={next} className={hasRight && css.pagination__active} onClick={onSelect}>{'›'}</Button>
+      <Button page={last} className={hasRight && css.pagination__active} onClick={onSelect}>Last</Button>
     </div>
   )
-}
-
-function range(incFrom, incTo) {
-  if (incTo < incFrom) return []
-  let length = incTo - incFrom + 1
-  return Array.from({ length }, (_, i) => incFrom + i)
 }
