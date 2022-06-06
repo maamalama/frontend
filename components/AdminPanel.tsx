968a31a0e05d390206e8af0a22bd53c28f5c56df
xamgore <xamgore@ya.ru>
2022-06-06 15:29:53 +0400

    Allow switching to the preferred nft by a URL param

--- components/AdminPanel.tsx
+++ components/AdminPanel.tsx
@@ -2,10 +2,21 @@ import styles from './AdminPanel.module.css'
 import Link from 'next/link'
 import shared from '../shared.module.css'
 import { NavLink } from './NavLink'
-import { $myNft } from '../models/me'
-import { useStore } from 'effector-react'
+import { setMyNftAddress } from '../models/me'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export const AdminPanel = ({ children }) => {
+  let router = useRouter()
+  let nft = router.query.nft as string
+
+  useEffect(() => {
+    if (!nft || !window) return
+    setMyNftAddress(nft)
+    let newUrl = window.location.href.slice(0, window.location.href.indexOf('?'))
+    window.history.replaceState(null, '', newUrl)
+  }, [nft])
+
   const holders = color => <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g opacity="0.9">
       <path d="M21 3H3C1.9 3 1 3.9 1 5V19C1 20.1 1.9 21 3 21H21C22.1 21 23 20.1 23 19V5C23 3.9 22.1 3 21 3ZM21 19H3V5H13V9H21V19Z" fill={color}/>
