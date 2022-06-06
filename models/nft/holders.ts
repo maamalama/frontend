1019336e7277f69781788343737f355821c0d94b
xamgore <xamgore@ya.ru>
2022-06-06 15:01:24 +0400

    Display the first NFT instead of avatar on /holders

--- models/nft/holders.ts
+++ models/nft/holders.ts
@@ -22,12 +22,11 @@ const fetchHoldersFx = createEffect({
       await Promise.all([
         fetch(`${BASE_URL}/nft/holders?token=${address}`).then(res => res.json()),
         fetch(`/token-uris/${address}.json`).then(res => res.json()).catch(() => []),
-        fetch(`https://randomuser.me/api/?results=30&seed=${address}&noinfo&inc=picture,username,login`).then(res => res.json()),
+        fetch(`https://randomuser.me/api/?results=30&seed=${address}&noinfo&inc=username,login`).then(res => res.json()),
       ])
 
     return holders.map((h, idx) => ({
       ...h, ...{
-        logo: h.logo ?? users?.[idx]?.picture?.thumbnail,
         discord: h.discord ?? users?.[idx]?.login?.username?.replace(/(\d+)/, '#$1'),
         tokens: [
           tokenUris[(idx * 3) % tokenUris.length],
